699baf4ef72d5504804733f31b5b0b158a2af062
Maurice Enschede <m.q.enschede@st.hanze.nl>
2021-06-15 03:19:59 +0200

    Update for compatibility

---
+++ init_master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/latest/salt-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/latest bionic main" | tee /etc/apt/sources.list.d/salt.list
+
+apt-get -y update
+apt-get -y install salt-master salt-ssh salt-api git
