5ff073a90d5dfb62180cfc8c574e0d9033d3fe9d
Maurice Enschede <m.q.enschede@st.hanze.nl>
2021-06-15 03:16:06 +0200

    Added init script

---
+++ init_minion.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/18.04/amd64/latest/salt-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/18.04/amd64/latest bionic main" | tee /etc/apt/sources.list.d/salt.list
+
+apt-get -y update
+apt-get -y install salt-minion salt-ssh salt-api
+
+echo 'master: 10.0.0.100' >> /etc/salt/minion
+systemctl restart salt-minion
